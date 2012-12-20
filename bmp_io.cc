1b7b45dd04ece6f0f4d4e55654c0af1dbe58012f
Yann Leprince <yann.leprince@u-psud.fr>
2012-12-20 09:21:39 +0100

    make reading the BMP header more robust

--- bmp_io.cc
+++ bmp_io.cc
@@ -5,6 +5,7 @@
 
 #include <cassert>
 #include <cerrno>
+#include <climits>
 #include <cstdlib>
 #include <cstring>
 #include <fstream>
@@ -15,55 +16,67 @@ using namespace std;
 namespace {
 
   // Substitute cstdint header declarations.
-  typedef unsigned short uint_least16_t;
   typedef short int16_t;
-  typedef unsigned int uint_least32_t;
-  typedef int int32_t;
-  typedef int int_least32_t;
+  typedef long int32_t;
+  typedef unsigned short uint_least16_t;
+  typedef unsigned long uint_least32_t;
+  typedef long int_least32_t;
+
+#if CHAR_BIT != 8
+#error This code assumes that chars are exactly 8 bit.
+#endif
+
+  void check_int_sizes() {
+    static bool ok = false;
+    if(!ok) {
+      assert(sizeof(uint_least16_t) >= 2);
+      assert(sizeof(int16_t) == 2);
+      assert(sizeof(uint_least32_t) >= 4);
+      assert(sizeof(int32_t) == 4);
+      assert(sizeof(int_least32_t) >= 4);
+      ok = true;
+    }
+  }
 
   uint_least16_t read_WORD(istream &stream)
   {
-    assert(sizeof(uint_least16_t) >= 2);
     uint_least16_t value = 0;
     value |= stream.get();
-    value |= static_cast<uint_least16_t>(stream.get()) << 8;
+    value |= static_cast<uint_least16_t>(stream.get()) << CHAR_BIT;
     return value;
   }
 
   uint_least32_t read_DWORD(istream &stream)
   {
-    assert(sizeof(uint_least32_t) >= 4);
     uint_least32_t value = 0;
     value |= stream.get();
-    value |= static_cast<uint_least32_t>(stream.get()) << 8;
-    value |= static_cast<uint_least32_t>(stream.get()) << 16;
-    value |= static_cast<uint_least32_t>(stream.get()) << 24;
+    value |= static_cast<uint_least32_t>(stream.get()) << CHAR_BIT;
+    value |= static_cast<uint_least32_t>(stream.get()) << (2 * CHAR_BIT);
+    value |= static_cast<uint_least32_t>(stream.get()) << (3 * CHAR_BIT);
     return value;
   }
 
   int16_t read_SHORT(istream &stream)
   {
-    assert(sizeof(int16_t) == 2);
     int16_t value = 0;
     value |= stream.get();
-    value |= static_cast<int16_t>(stream.get()) << 8;
+    value |= static_cast<int16_t>(stream.get()) << CHAR_BIT;
     return value;
   }
   int32_t read_LONG(istream &stream)
   {
-    assert(sizeof(int32_t) == 4);
     int32_t value = 0;
     value |= stream.get();
-    value |= static_cast<int32_t>(stream.get()) << 8;
-    value |= static_cast<int32_t>(stream.get()) << 16;
-    value |= static_cast<int32_t>(stream.get()) << 24;
+    value |= static_cast<int32_t>(stream.get()) << CHAR_BIT;
+    value |= static_cast<int32_t>(stream.get()) << (2 * CHAR_BIT);
+    value |= static_cast<int32_t>(stream.get()) << (3 * CHAR_BIT);
     return value;
   }
 
 } // empty namespace
 
 
-BmpData::BmpData(string const &file_name)
+BmpData::BmpData(const string &file_name)
 {
   ifstream stream(file_name.c_str(), ios_base::in | ios_base::binary);
 
@@ -74,6 +87,29 @@ BmpData::BmpData(string const &file_name)
     exit(EXIT_FAILURE);
   }
 
+  bool read_lines_backward = read_header(file_name, stream);
+
+  size_t line_size = (width() * 3 + 3) % 4;
+
+  try {
+    
+  } catch(const ios_base::failure &exception) {
+    if(stream.eof()) {
+      clog << "Error reading the BMP image data in '" << file_name
+           << "': premature end of file, aborting." << endl;
+    } else {
+      clog << "Error reading the BMP image data in '" << file_name << "' ("
+           << exception.what() << "), aborting." << endl;
+    }
+    exit(EXIT_FAILURE);
+  }
+
+  stream.close();
+}
+
+bool BmpData::read_header(const std::string &file_name, istream &stream)
+{
+  check_int_sizes();
   assert(stream.good());
   try {
     stream.exceptions(ios_base::eofbit | ios_base::failbit | ios_base::badbit);
@@ -89,22 +125,35 @@ BmpData::BmpData(string const &file_name)
     stream.ignore(8);
 
     // File offset 10, read BitmapOffset (in endian-independent fashion).
-    uint_least32_t data_offset = read_DWORD(stream);
+    size_t data_offset = read_DWORD(stream);
 
     // File offset 14, beginning of second header (Bitmap Header).
     // File offset 14, read size of header.
-    uint_least32_t size_of_header = read_DWORD(stream);
+    size_t size_of_header = read_DWORD(stream);
 
-    assert(sizeof(int_least32_t) >= 4);
-    int_least32_t width, height;
+    int_least32_t int_width, int_height;
     if(size_of_header == 12) {
       // File offset 18, read width and height.
-      width = read_SHORT(stream);
-      height = read_SHORT(stream);
+      int_width = read_SHORT(stream);
+      int_height = read_SHORT(stream);
     } else {
       // File offset 18, read width and height.
-      width = read_LONG(stream);
-      height = read_LONG(stream);
+      int_width = read_LONG(stream);
+      int_height = read_LONG(stream);
+    }
+
+    if(int_width <= 0) {
+      clog << "Invalid BMP file '" << file_name
+           << "': width is negative, aborting." << endl;
+    }
+    m_width = int_width;
+    if(int_height > 0) {
+      m_height = int_height;
+    } else if(int_height < 0) {
+      m_height = -int_height;
+    } else {
+      clog << "Invalid BMP file '" << file_name
+           << "': height is zero, aborting." << endl;
     }
 
     // File offset [22 / 26], ignore Planes field.
@@ -130,20 +179,11 @@ BmpData::BmpData(string const &file_name)
       // File offset 30, past any interesting header field.
       stream.ignore(data_offset - 30);
     }
+
+    return int_height < 0;
   } catch(const ios_base::failure &exception) {
     clog << "Error reading the BMP header of '" << file_name << "' ("
          << exception.what() << "), aborting." << endl;
     exit(EXIT_FAILURE);
   }
-
-  try {
-    stream.exceptions(ios_base::failbit | ios_base::badbit);
-
-  } catch(const ios_base::failure &exception) {
-    clog << "Error reading the BMP image data in '" << file_name << "' ("
-         << exception.what() << "), aborting." << endl;
-    exit(EXIT_FAILURE);
-  }
-
-  stream.close();
 }
--- bmp_io.hh
+++ bmp_io.hh
@@ -2,6 +2,7 @@
 #define _BMP_IO_HH_INCLUDED_
 
 #include <string>
+#include <iosfwd>
 
 class BmpData
 {
@@ -9,16 +10,24 @@ public:
   /* Load the named BMP file. In case of failure a message is printed and the
      program is exited. */
   explicit BmpData(std::string const &file_name);
+  ~BmpData();
 
-  unsigned int size_x() const {return m_size_x;};
-  unsigned int size_y() const {return m_size_y;};
+  size_t width() const {return m_width;};
+  size_t height() const {return m_height;};
 
   const unsigned char * data() const {return m_data;};
   unsigned char * data() {return m_data;};
 
 private:
-  unsigned int m_size_x, m_size_y;
+  size_t m_width, m_height;
   unsigned char *m_data;
+
+  // Prevent copy and assignment
+  BmpData(const BmpData &);
+  BmpData & operator=(const BmpData &);
+
+  // Helper method for internal use only
+  bool read_header(const std::string&, std::istream&);
 };
 
 #endif // !defined(_BMP_IO_HH_INCLUDED_)
